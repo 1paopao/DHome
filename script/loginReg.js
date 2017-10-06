73fa005082284d547b40232bf283bec69404f1b9
Zou Lei <klwjx520888@163.com>
2017-10-06 17:15:31 +0800

    更新‘
    git commit -m 更新‘

--- script/loginReg.js
+++ script/loginReg.js
@@ -94,6 +94,8 @@ export default{
 		})
 		
 		
+		//发送验证码
+		
 		
 	}
 	
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
+var webpack = require("webpack");
+var path = require("path");
 module.exports = {
 	entry:"./app.js",//入口文件(与该文件同级目录)
 	output:{
 		path:__dirname,//输出的目录
-		filename: 'bundle.js',//输出的文件名称
-		publicPath:''
+		filename: 'bundle.js'//输出的文件名称
 	},
 	devtool:"source-map",//便于调试，相当于命令行中 webpack --devtool source-map
  	module: {//依赖的模块
@@ -38,7 +39,10 @@ module.exports = {
         },
         {//背景图片
         	test:/\.(jpg|png|gif)$/,
-        	loader:"url-loader?limit=1024&name=./imgs/[name].[ext]"
+        	loader:"url-loader?limit=1024&name=[name].[ext]",
+        	options:{
+				 publicPath:'/'
+			}
         },
         {//字体文件
         	test:/\.(eot|woff|svg|ttf|woff2|gif|appcache)(\?|$)/,
@@ -46,5 +50,12 @@ module.exports = {
         }
 	
         ]
-    }
+ 	},
+	/* plugins:[
+	    new webpack.ProvidePlugin({
+	      $:"jquery",
+	      jQuery:"jquery",
+	      "window.jQuery":"jquery"
+	    })
+	  ]*/
 }

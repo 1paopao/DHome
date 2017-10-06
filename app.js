86a13f3a26070826fc1052bf48115310f0b05832
sun-guo <1129113591@qq.com>
2017-10-06 17:08:48 +0800

    123456

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import './scss/compdetail.scss';
 import './scss/home.scss';
 import './scss/loginReg.scss';
 import './scss/termsOfService.scss';
+import './scss/riji.scss';
 import $ from 'n-zepto';
 import LoginReg from './script/loginReg.js';
 LoginReg.login();

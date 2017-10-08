9cf57a389de855e65c87c0e761c1bcb32b5ac914
pp <123@qq.com>
2017-10-08 16:04:49 +0800

    pao

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 /*main.css*/
+import $ from 'n-zepto';
 import './scss/complist.scss';
 import './scss/compdetail.scss';
 import './scss/rijidetail.scss';
@@ -14,9 +15,18 @@ import './scss/loginReg.scss';
 import './scss/termsOfService.scss';
 import './scss/decorationRaiders.scss';
 import './scss/riji.scss';
+//import './scss/fatie.scss';
 import LoginReg from './script/loginReg.js';
 LoginReg.login();
 LoginReg.loginyanzheng();
+
+if(sessionStorage.getItem('username')){
+	$('#log .change').html('<em>&#xe6cb;</em>'+'欢迎'+sessionStorage.getItem('username'));
+	$('#log .change').attr('href','view/fatie.html');
+	$('#log .change1').html('<em>&#xe6cb;</em>'+'欢迎'+sessionStorage.getItem('username'));
+	$('#log .change1').attr('href','fatie.html');
+}
+
 $('#log a').on('click',function(){
 	var i = $(this).index();
 	localStorage.setItem('num',i);
